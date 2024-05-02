8982146fbfc2ff425b279d2fafba00503d571e15
Robert Rosenquist <rrosenq@kth.se>
2024-05-02 10:23:11 +0200

    Added auth

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,9 @@ const { invoke } = window.__TAURI__.tauri;
 const { listen } = window.__TAURI__.event;
 const { message } = window.__TAURI__.dialog;
 
-function connectServer(ip) {
+function connectServer(ip, user, pass) {
     return new Promise((resolve, reject) => {
-        invoke('connect_server', { ip: ip })
+        invoke('connect_server', { ip: ip, username: user, password: pass })
             .then(() => {
                 resolve();
             })
@@ -14,6 +14,6 @@ function connectServer(ip) {
     });
 }
 
-function handleConnection(ip) {
-    connectServer(ip).then(()=>{window.location.href="/chat.html"}).catch(e=>{console.error(e); message(e, { title: 'Tauri', type: 'error' })});
+function handleConnection(ip, user, pass) {
+    connectServer(ip, user, pass).then(()=>{window.location.href="/chat.html"}).catch(e=>{console.error(e); message(e, { title: 'Tauri', type: 'error' })});
 }
