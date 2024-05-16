bd3c08a5beb5b13919edbca22b78539e75e4756c
Robert Rosenquist <rrosenq@kth.se>
2024-05-16 13:41:23 +0200

    Added basic non-functional frontend to tauri

---
+++ client-with-frontend/src/pages/ServerPage.tsx
@@ -0,0 +1,13 @@
+
+function ServerPage() {
+  return (
+    <div className="Buttons">
+      <button id="one">Server 1</button>
+      <button id="two">Server 2</button>
+      <button id="three">Server 3</button>
+      <button id="four">New server</button>
+    </div>
+  )
+}
+
+export default ServerPage
